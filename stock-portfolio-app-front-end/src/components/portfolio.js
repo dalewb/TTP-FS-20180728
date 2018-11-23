a799a792614630e318ff7bbb0598b8b6640827aa
William Dale <williambdale@gmail.com>
2018-11-23 14:46:30 -0500

    Add react router folder, approuter file

--- stock-portfolio-app-front-end/src/components/portfolio.js
+++ stock-portfolio-app-front-end/src/components/portfolio.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 
 class Portfolio extends Component {
-  
+  render() {
+    return (
+      <p>Portfolio Page</p>
+    )
+  }
 }
 
 export default Portfolio;
---
+++ stock-portfolio-app-front-end/src/routers/AppRouter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+const AppRouter = () => (
+  <BrowserRouter>
+    <div>
+      
+    </div>
+  </BrowserRouter>
+)
