b248168b38074c89a97b201af8f4e70c94bb718d
William Dale <williambdale@gmail.com>
2018-11-23 18:23:39 -0500

    Add create method to user

--- stock_portfolio_app_back_end/stock_portfolio_api/app/controllers/api/v1/users_controller.rb
+++ stock_portfolio_app_back_end/stock_portfolio_api/app/controllers/api/v1/users_controller.rb
@@ -4,5 +4,11 @@ class Api::V1::UsersController < ApplicationController
     user = User.find(params[:id])
     render json: {status: 'SUCCESS', message: 'Loaded user', data: user}, status: :ok
   end
-  
+
+  private
+
+  def article_params
+    params.permit(:name, :email, :password)
+  end
+
 end
