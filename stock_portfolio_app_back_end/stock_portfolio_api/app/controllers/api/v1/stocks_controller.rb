5ade2d63270cf39c8d3529906626bbb126715cd9
William Dale <williambdale@gmail.com>
2018-11-24 15:52:20 -0500

    Add CRUB functionsality to Stock controller

--- stock_portfolio_app_back_end/stock_portfolio_api/app/controllers/api/v1/stocks_controller.rb
+++ stock_portfolio_app_back_end/stock_portfolio_api/app/controllers/api/v1/stocks_controller.rb
@@ -1,2 +1,32 @@
 class Api::V1::StocksController < ApplicationController
+  def index
+    stocks = Stock.all
+    render json: status: 'SUCCESS', message: 'Loaded stocks', data: stocks}, status: :ok
+  end
+
+  def show
+    stock = Stock.find(params[:id])
+    render json: {status: 'SUCCESS', message: 'Loaded stock', data: stock}, status: :ok
+  end
+
+  def create
+    stock = Stock.new(stock_params)
+    if stock.save
+      render json: {status: 'SUCCESS', message: 'Saved stock', data: stock}, status: :ok
+    else
+      render json: {status: 'ERROR', message: 'Stock not saved', data: stock.errors}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    stock = Stock.find(params[:id])
+    stock.destroy
+    render json: {status: 'SUCCESS', message: 'Deleted stock', data: stock}, status: :ok
+  end
+
+  private
+
+  def stock_params
+    params.permit(:symbol, :current_price)
+  end
 end
