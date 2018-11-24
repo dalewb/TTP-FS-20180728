b91685a2778d9aeea6d180b64448f1a3c9208f1d
William Dale <williambdale@gmail.com>
2018-11-24 15:23:07 -0500

    Add destroy and index methods for user

--- stock_portfolio_app_back_end/stock_portfolio_api/app/controllers/api/v1/users_controller.rb
+++ stock_portfolio_app_back_end/stock_portfolio_api/app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,27 @@
 class Api::V1::UsersController < ApplicationController
+  def index
+    users = User.all
+    render json: {status: 'SUCCESS', message: 'Loaded users', data: users}, status: :ok
+  end
 
   def show
     user = User.find(params[:id])
     render json: {status: 'SUCCESS', message: 'Loaded user', data: user}, status: :ok
+  end
+
+  def create
+    user = User.new(article_params.merge({account: 5000}))
+    if user.save
+      render json: {status: 'SUCCESS', message: 'Saved user', data: user}, status: :ok
+    else
+      render json: {status: 'ERROR', message: 'User not saved', data: user.errors}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    render json: {status: 'SUCCESS', message: 'Deleted user', data: user}, status: :ok
   end
 
   private
