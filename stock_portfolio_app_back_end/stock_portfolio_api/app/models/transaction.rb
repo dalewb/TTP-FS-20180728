ee3ceac67ea09bb5318c6ff83ab898a012ee2f4c
William Dale <williambdale@gmail.com>
2018-11-24 15:54:49 -0500

    Add relationships to all models

--- stock_portfolio_app_back_end/stock_portfolio_api/app/models/transaction.rb
+++ stock_portfolio_app_back_end/stock_portfolio_api/app/models/transaction.rb
@@ -1,4 +1,7 @@
 class Transaction < ApplicationRecord
     validates :price, presence: true
     validates :number_of_shares, presence: true
+
+    belongs_to :user
+    belongs_to :stock
 end
--- stock_portfolio_app_back_end/stock_portfolio_api/app/models/user.rb
+++ stock_portfolio_app_back_end/stock_portfolio_api/app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :password, presence: true
   validates :name, presence: true
+
+  has_many :transactions
 end
