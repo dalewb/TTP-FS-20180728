76d3dcece5975800a435e999539705214135c2b7
William Dale <williambdale@gmail.com>
2018-11-25 19:29:05 -0500

    Set up registration form and logic for password matching with errors

--- stock-portfolio-app-front-end/src/components/register.js
+++ stock-portfolio-app-front-end/src/components/register.js
@@ -1,9 +1,85 @@
 import React, { Component } from 'react';
 
 class Register extends Component {
+  constructor() {
+    super()
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+      errors: '',
+    }
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    if (this.state.password !== this.state.confirmPassword) {
+      this.setState({
+        password: '',
+        confirmPassword: '',
+        errors: "Passwords must match"
+      })
+    } else {
+      console.log(this.state)
+      this.setState({
+        errors: ''
+      })
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+
+  }
+
   render() {
     return (
-      <p>Registration Page</p>
+      <div>
+        <h3>Registration Page</h3>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input
+              name="name"
+              type="text"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Email:
+            <input
+              name="email"
+              type="text"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Password:
+            <input
+              name="password"
+              type="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Confirm Password:
+            <input
+              name="confirmPassword"
+              type="password"
+              value={this.state.confirmPassword}
+              onChange={this.handleChange}
+            />
+          </label>
+          <input type="submit" value="Submit"/>
+        </form>
+        {this.state.errors.length > 0 && <p>{this.state.errors}</p>}
+      </div>
     )
   }
 }
